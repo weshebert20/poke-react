7c35524efc9a322a023f218d1969e4203e93d0da
Wesley Hebert <whebert@medianewsgroup.com>
2020-06-09 10:14:28 -0600

    Adding first round of code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import "./App.css";
+import PokeCard from './components/Cards/pokemon-card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function searchingFor(term) {
+    return function(x) {
+        return x.name.toLowerCase().includes(term.toLowerCase()) || !term;
+    }
+}
+
+function searchingWeakness(weaknesses) {
+    return function(x) {
+        return x.weaknesses.map(weakValue => {
+            return weakValue.toLowerCase().includes(weakValue.toLowerCase()) || !weaknesses;
+        })
+    }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        pokemons : [],
+        term : '',
+        weaknesses: '',
+    }
+    this.searchHandler = this.searchHandler.bind(this);
+    this.searchHandlerWeak = this.searchHandlerWeak.bind(this);
+  }
+  searchHandler(event) {
+    this.setState({term: event.target.value})
+  }
+
+    searchHandlerWeak(event) {
+        this.setState({weaknesses: event.target.value})
+    }
+  componentDidMount() {
+    let url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          if (data) {
+            this.setState({pokemons : data.pokemon}, () => {})
+          }
+        })
+        .catch(console.log)
+  }
+
+  render() {
+    const {pokemons, term, weaknesses} = this.state;
+
+    const renderedPokemonList = pokemons.filter(searchingFor(term)).map((pokemon, index) => {
+      return (<PokeCard pokemon={pokemon} />);
+    });
+
+    const renderedweaknessList = pokemons.filter(searchingWeakness(weaknesses)).map((pokemon, index) => {
+        return (<PokeCard pokemon={pokemon} />);
+    })
+
+    return (
+        <div>
+        <form>
+            <input type="text"
+                onChange={this.searchHandler}
+            />
+            <label>
+                Weakness:
+                <select onChange={this.searchHandlerWeak}>
+                    <option value="fire">Fire</option>
+                    <option value="ice">Ice</option>
+                    <option value="flying">Coconut</option>
+                    <option value="Psychic">Psychic</option>
+                </select>
+            </label>
+        </form>
+
+        <div className="container">
+            <div className="card-columns">
+                {renderedPokemonList}
+                {renderedweaknessList}
+            </div>
+        </div>
+        </div>
+    );
+  }
 }
 
 export default App;
